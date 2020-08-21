c6df81dcbe7778295b0369543fe5b978d73feb42
Mark <m.cov46@icloud.com>
2020-08-21 15:25:30 +0100

    size button added but not working, grid working

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-
-document.getElementById('test').addEventListener("mouseover", mouseOver);
-
-function mouseOver() {
-  document.getElementById('test').style.backgroundColor = 'red';
-}
+let board = document.querySelector('#board');
+let boardGrid = document.querySelector('#boardGrid');
+let sizeBtn = document.querySelector('.sizeBtn');
+let gridSize = 16;
 
 
 
+function makeGrid() {
 
+  boardGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  boardGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
+  for (let i = 0; i < gridSize * gridSize; i++) {
 
-const resetBtn =document.querySelector('.resetBtn');
+  let colorChange = document.createElement('div');
+  colorChange.classList = '.colorChange';
+
+  colorChange.addEventListener('mouseover', mouseOver);
+  function mouseOver() {
+  colorChange.style.backgroundColor = 'red';
+  }
+
+  boardGrid.appendChild(colorChange);
+
+  const resetBtn =document.querySelector('.resetBtn');
     resetBtn.addEventListener('click', () => {
-        document.getElementById('test').style.background = 'cornsilk';
+        colorChange.style.background = 'cornsilk';
     });
+
+  }
+
+};
+
+
+makeGrid();
